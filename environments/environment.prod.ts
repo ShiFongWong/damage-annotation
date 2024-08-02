9409aed7c3e712c8755eb00fdfbbccaadb8d8757
WongShiFong <shifong.wong@57codebox.com>
2024-08-02 14:09:14 +0800

    generate service

---
+++ environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  // apiUrl: 'https://api.connextion.my',
+  // imagePath: 'https://api.connextion.my',
+  apiUrl: 'https://api.connextionapps.com',
+  imagePath: 'https://api.connextionapps.com',
+  webUrl:'https://tenant.connextionapps.com',
+  dialogTopOffset: 250
+};
